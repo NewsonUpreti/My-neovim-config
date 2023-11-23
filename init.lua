a84ced151d56715f37674d58d5759ad3798bbe27
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2023-11-23 13:11:07 +0530

    Mouse functionality turned off

--- init.lua
+++ init.lua
@@ -254,7 +254,7 @@ vim.o.hlsearch = false
 vim.wo.number = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = ''
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
