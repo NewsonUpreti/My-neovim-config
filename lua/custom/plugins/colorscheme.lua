6382a7303e856f806548af61f9be4dfd2ee64295
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2023-12-16 21:19:13 +0530

    new configuration loaded

--- lua/custom/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = true,
-  priority = 1000,
-  config = function()
-require("catppuccin").setup {}
-  end,
-}
---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,27 @@
+return    {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+require("nvim-tree").setup({
+
+vim.api.nvim_set_keymap('n', '<Space>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true }),
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 50,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+     end
+}
