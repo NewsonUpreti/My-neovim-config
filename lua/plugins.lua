1b152f3109dfb8776942659cfee49c6ba5b864ff
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2023-12-17 14:11:32 +0530

    new tweaks slightly

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,17 +6,6 @@ return {
 	},
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
-	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = function()
