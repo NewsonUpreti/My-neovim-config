122e452a1490abdb318fda98dbf4fa0fec72a3cd
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2023-12-19 17:08:43 +0530

    harpoon nav with mark number

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -33,5 +33,40 @@ return {
 			"<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
 			{ desc = "Toggling Harpoon quick Menu" }
 		)
+
+		keymap.set(
+			"n",
+			"<leader>1",
+			"<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
+			{ desc = "Toggling Harpoon quick Menu" }
+		)
+
+		keymap.set(
+			"n",
+			"<leader>2",
+			"<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
+			{ desc = "Toggling Harpoon quick Menu" }
+		)
+
+		keymap.set(
+			"n",
+			"<leader>3",
+			"<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
+			{ desc = "Toggling Harpoon quick Menu" }
+		)
+
+		keymap.set(
+			"n",
+			"<leader>4",
+			"<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
+			{ desc = "Toggling Harpoon quick Menu" }
+		)
+
+		keymap.set(
+			"n",
+			"<leader>5",
+			"<cmd>lua require('harpoon.ui').nav_file(5)<cr>",
+			{ desc = "Toggling Harpoon quick Menu" }
+		)
 	end,
 }
