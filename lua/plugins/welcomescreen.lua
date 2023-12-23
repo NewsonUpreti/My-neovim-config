1543a3ec1dc9ab497742c9e73350724407556260
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2023-12-23 11:25:13 +0530

    WElcome screen mod

--- lua/plugins/welcomescreen.lua
+++ lua/plugins/welcomescreen.lua
@@ -1,7 +1,9 @@
 return {
 	"startup-nvim/startup.nvim",
-	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	config = function()
-		require("startup").setup()
+		require("startup").setup({
+			theme = "evil",
+		})
 	end,
 }
