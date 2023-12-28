134fa8cfa5b59a6ad8f002859a59c93788f8e490
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2023-12-29 00:17:59 +0530

    basic tweaks

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to telescope to change theme, layout, etc.
 			require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
+				-- winblend = 10,
 				previewer = false,
 			}))
 		end, { desc = "[/] Fuzzily search in current buffer" })
