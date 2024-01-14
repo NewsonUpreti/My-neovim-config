11f0548f5210aaf1604dcd1bd356f4fbb08ca17a
Newson Upreti <117501417+NewsonUpreti@users.noreply.github.com>
2024-01-14 20:25:34 +0530

    added diagnostic toggler

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -33,3 +33,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+--Added a diagnostic toggle command for removing and getting diagnostics back when needed
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config({
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	})
+end, { desc = "toggle diagnostic" })
