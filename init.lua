d3cc9495710c51f66c2f61016aa447cb88228171
NewsonUpreti <upnewson@gmail.com>
2024-05-16 18:37:52 +0530

    tmux and nvim integration

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.o.shell = "/bin/zsh"
 
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,7 +5,7 @@ vim.api.nvim_set_keymap("n", "<C-s>", ":set hlsearch!<CR>", { noremap = true, si
 vim.api.nvim_set_keymap("n", "<up>", "<nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<down>", "<nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<right>", "<nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<left>", "<nop>", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap(
 	"n",
 	"<Space>p",
@@ -60,14 +60,14 @@ vim.api.nvim_set_keymap("n", "<S-BS>", ":tabprevious<CR>", { noremap = true, sil
 --[[ vim.api.nvim_set_keymap("n", "<C-l>", ":registers<CR>", { noremap = true, silent = true }) ]]
 
 -- select all keymap for normal mode
-vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
 
 -- Exploring windows splits with shortcuts
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-;>", "<C-w>w", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-;>", "<C-w>w", { noremap = true, silent = true })
 
 -- terminal toggler keymap for normal.insert and terminal mode with ctrl + \ key
 vim.api.nvim_set_keymap("n", "<C-\\>", ":ToggleTerm<CR>", { noremap = true, silent = true })
