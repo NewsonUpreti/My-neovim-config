689b64d55dc7720c58a4fc0d2032cfd32a293ada
NewsonUpreti <upnewson@gmail.com>
2024-05-27 11:30:34 +0530

    toggling conflict with tmux fixed

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -82,5 +82,5 @@ vim.api.nvim_set_keymap("n", "-", "<C-x>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
 
 -- Github copilot chat keymaps
-vim.api.nvim_set_keymap("n", "<C-9>", ":CopilotChatOpen<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-0>", ":CopilotChatToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<f9>", ":CopilotChatOpen<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<f8)>", ":CopilotChatToggle<CR>", { noremap = true, silent = true })
