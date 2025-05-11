56f7063981fc996b75b14802269de27bdd6cf9d0
NewsonUpreti <upnewson@gmail.com>
2025-05-11 20:33:02 +0530

    no more conflicts

--- lua/plugins/nvim-visual-multi.lua
+++ lua/plugins/nvim-visual-multi.lua
@@ -2,7 +2,7 @@ return {
   "mg979/vim-visual-multi",
   config = function()
     -- make Enter do the same thing (in both Normal and Extend-mode)
-    vim.keymap.set({ 'n', 'x' }, '<CR>',
+    vim.keymap.set({ 'n', 'x' }, '\\',
       '<Plug>(VM-Add-Cursor-At-Pos)',
       { silent = true }
     )
