551bb17a3a15b54dc6cd4699d0896591e7f28c77
NewsonUpreti <upnewson@gmail.com>
2025-06-25 09:36:24 +0530

    toggle term config and tweaks

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -36,7 +36,7 @@ return{
   },
   init = function()
     -- The following options are recommended when layout == "float"
-    vim.opt.wrap = false
+    vim.opt.wrap = true
     vim.opt.sidescrolloff = 36 -- Set a large value
 
     --- Put your configuration here
--- lua/plugins/toogleterm.lua
+++ lua/plugins/toogleterm.lua
@@ -1,1 +1,14 @@
-return { "akinsho/toggleterm.nvim", version = "*", config = true }
+return { "akinsho/toggleterm.nvim", version = "*",
+  config = function()
+require('toggleterm').setup{
+    direction = 'horizontal',
+      float_opts = {
+        border = 'curved'
+      },
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_mode = true
+  }
+  end
+}
