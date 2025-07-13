540863e923731d2bbf4f878194de021e71bcf4e5
NewsonUpreti <upnewson@gmail.com>
2025-07-13 12:25:19 +0530

    C++ snippet update

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -7,8 +7,14 @@ return {
   s("cpp", {
     t({
       "#include<iostream>",
+      "#include<string>",
+      "#include<vector>",
+      "#include<math.h>",
       "using namespace std;",
       "using ll = long long;",
+      "using d = double;",
+      "using f = float;",
+      "using s = string;",
       "",
       "int main(void) {",
       "    ",
