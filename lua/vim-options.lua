c6f4dbccdbec8387905873151dff549ee9738099
NewsonUpreti <upnewson@gmail.com>
2025-07-26 00:30:27 +0530

    refactor

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,11 +1,3 @@
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 vim.o.undofile = true
 vim.wo.signcolumn = "yes"
 vim.o.clipboard = "unnamedplus"
