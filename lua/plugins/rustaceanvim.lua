0d507fb3d4536ae753dcac840217092f6506377b
NewsonUpreti <upnewson@gmail.com>
2025-12-27 17:24:48 +0530

    rust lsp configuration

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,5 +1,41 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^6", -- Recommended
-	lazy = true, -- This plugin is already lazy
+  "mrcjkb/rustaceanvim",
+  version = "^6",
+  lazy = true,
+  ft = { "rust" }, -- load only for Rust files
+  config = function()
+    -- Global diagnostic settings (show inline errors)
+    vim.opt.signcolumn = "yes"
+    vim.diagnostic.config({
+      virtual_text = {
+        spacing = 2,
+        prefix = "●",
+      },
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
+    })
+
+    -- rust-analyzer configuration
+    vim.g.rustaceanvim = {
+      server = {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = {
+              command = "check",
+            },
+            diagnostics = {
+              enable = true,
+            },
+          },
+        },
+      },
+    }
+  end,
 }
