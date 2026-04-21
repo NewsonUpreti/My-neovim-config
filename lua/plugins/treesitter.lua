27c3ca2caf2e0c19a5ff06a43ee2213341d65d94
Newson upreti <newson@Newsons-MacBook-Pro.local>
2026-04-21 23:24:15 +0530

    branch issues: fixed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+  branch = "master",
 	config = function()
 		vim.defer_fn(function()
 			require("nvim-treesitter.configs").setup({
